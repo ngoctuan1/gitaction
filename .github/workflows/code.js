75158d8b32c2e272ea66290e1114513a13f77fef
ngoctuan1 <72248343+ngoctuan1@users.noreply.github.com>
2021-06-20 21:41:43 +0700

    Create code.js

---
+++ .github/workflows/code.js
@@ -0,0 +1,24 @@
+const axios = require("axios");
+
+const msgs = [
+    "hello cau"
+]
+
+// ngày bắt đầu thực hiện gửi tin nhắn
+const START_DATE = new Date("2021-06-19");
+const _MS_PER_DAY = 1000 * 60 * 60 * 24;
+
+function diffDate() {
+    const now = new Date();
+    const utc1 = Date.UTC(START_DATE.getFullYear(), START_DATE.getMonth(), START_DATE.getDate());
+    const utc2 = Date.UTC(now.getFullYear(), now.getMonth(), now.getDate());
+    return Math.floor((utc2 - utc1) / _MS_PER_DAY);
+}
+
+const sendMessage = async () => {
+    const i = diffDate();
+    let url = "https://mighty-reaches-66596.herokuapp.com/"
+    const { data } = await axios.get(url, { params: { uid: "100011341945915", msg: msgs[i]} });
+};
+
+sendMessage();
